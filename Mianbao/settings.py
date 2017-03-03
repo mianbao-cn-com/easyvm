2da33ea5c40c2797417691a460f1e7d93a0e4efa
面包 <hanlingfei5@163.com>
2017-03-03 16:21:48 +0800

    优化了Django的DEBUG模式

--- Mianbao/settings.py
+++ Mianbao/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*c0fa)z+mtnq$2r#8h&4hkl^=xz03j^0wl0fg@5#fs0iy9r&an'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
